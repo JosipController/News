cdd5c9ed63898f6095d0391f2dadcbce23f83054
Josip <52865242+JosipController@users.noreply.github.com>
2021-11-02 15:23:01 +0100

    Update SearchAutomation.java

--- news/src/SearchAutomation.java
+++ news/src/SearchAutomation.java
@@ -49,6 +49,16 @@ public class SearchAutomation {
         MobileElement el5 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.TextView[1]");
         el5.click();
 
+        Thread.sleep(3000);
+
+        String CurrentActivity = driver.currentActivity();
+
+        if (CurrentActivity.equals(".WebViewBrowserActivity")) {
+            System.out.println("Headline was opened in browser successfully!"); }
+        else {
+            System.out.println("Test failed");
+
+        }
 
 
 
